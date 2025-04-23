cd106917309fe00a1c05f57d61e1f5e6891e4818
Idd mohammad <mohammadidd345@gmail.com>
2025-04-23 16:39:42 +0530

    update and delete operation

--- Student/src/main/java/com/Student/StudentService.java
+++ Student/src/main/java/com/Student/StudentService.java
@@ -36,5 +36,41 @@ public class StudentService {
 			return null;
 		}
 	}
+	
+	//update Operation
+	public Student updateStudent(Long studentid,Student student) {
+		try (Session session=factory.openSession()){
+			Transaction transaction = session.beginTransaction();
+			Student oldStudent = session.get(Student.class, studentid);
+			
+			if (oldStudent != null) {
+				oldStudent.setS_name(student.getS_name());
+				oldStudent.setS_fathername(student.getS_fathername());
+				oldStudent.setS_phone(student.getS_phone());
+				oldStudent.setS_college(student.getS_college());
+				oldStudent.setS_active(student.isS_active());
+				session.merge(oldStudent);
+			}
+			transaction.commit();
+			return oldStudent;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	//Delete Operation
+	public void deleteStudent(Long StudentId) {
+		
+		try(Session session=factory.openSession()){
+			Transaction beginTransaction = session.beginTransaction();
+			Student student = session.get(Student.class, StudentId);
+			if (student !=null) {
+				session.remove(student);
+			}
+			beginTransaction.commit();
+		}
+		
+	}
 
 }
